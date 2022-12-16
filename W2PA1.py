97572c118dcbda79bba89afc1596446e5feab916
DxRao <tdayakarrao@gmail.com>
2022-12-16 23:41:12 +0530

    Week 2: Programming Assignments

---
+++ W2PA1.py
@@ -0,0 +1,4 @@
+# Prints whatever you input as a string of characters.
+
+s = input()
+print(s, end ="")
---
+++ W2PA3.py
@@ -0,0 +1,6 @@
+S = input()
+A = int(input())
+if(A > 0):
+    for i in range(A-1):
+  	print(S)
+	print(S, end = "")
